bf5eb5be371b66f629449a7c16d593090bc51d2b
Oksana Luzhniak <luzhnyako@gmail.com>
2020-09-14 21:52:03 +0300

    fibonachi

---
+++ fib/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    public static int Main()
+    {
+        Console.Write("Enter number from 1 up : ");
+        int end = int.Parse(Console.ReadLine());
+        int j = 1;
+        for (int i = 1; i <= end; i += j)
+        {
+            Console.Write("{0} ", i);
+            j = i - j;
+        }
+        Console.ReadKey();
+        return 0;
+    }
+}
